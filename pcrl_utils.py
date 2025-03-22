6180da8bee036d28443c70a99eec4c88fccc81ba
ANHUN <2514642620@qq.com>
2025-03-22 09:43:36 +0800

    修复预处理逻辑

--- pcrl_utils.py
+++ pcrl_utils.py
@@ -4,6 +4,9 @@ import os
 import torch
 import random
 from PIL import ImageFilter
+from pathlib import Path
+
+
 def get_chest_list(txt_path, data_dir):
     image_names = []
     labels = []
@@ -20,40 +23,71 @@ def get_chest_list(txt_path, data_dir):
 
 
 def get_luna_pretrain_list(ratio):
+    """获取特定 file_txt 文件中所有的示例名称
+
+    Args:
+        ratio (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     x_train = []
-    with open('train_val_txt/luna_train.txt', 'r') as f:
+    with open("net/pretrain/PCRLv2/train_val_txt/pretrain_train.txt", "r") as f:
         for line in f:
-            x_train.append(line.strip('\n'))
-    return x_train[:int(len(x_train) * ratio)]
+            x_train.append(line.strip("\n"))
+    return x_train[: int(len(x_train) * ratio)]
 
 
 def get_luna_finetune_list(ratio, path, train_fold):
     x_train = []
-    with open('train_val_txt/luna_train.txt', 'r') as f:
+    with open("train_val_txt/luna_train.txt", "r") as f:
         for line in f:
-            x_train.append(line.strip('\n'))
-    return x_train[int(len(x_train) * ratio):]
+            x_train.append(line.strip("\n"))
+    return x_train[int(len(x_train) * ratio) :]
 
 
 def get_luna_list(config, train_fold, valid_fold, test_fold, suffix, file_list):
+    """获取训练数据和验证数据路径列表。
+
+    Args:
+        config (_type_): arg
+        train_fold (list[int]): _description_
+        valid_fold (_type_): _description_
+        test_fold (_type_): _description_
+        suffix (str): 通常是 __global__
+        file_list (_type_): 给定的训练数据名称列表
+
+    Returns:
+        _type_: _description_
+    """
     x_train = []
     x_valid = []
     x_test = []
-    for i in train_fold:
-        for file in os.listdir(os.path.join(config.data, 'subset' + str(i))):
-            if suffix in file:
-                if file_list is not None and file.split('_')[0] in file_list:
-                    x_train.append(os.path.join(config.data, 'subset' + str(i), file))
-                elif file_list is None:
-                    x_train.append(os.path.join(config.data, 'subset' + str(i), file))
-    for i in valid_fold:
-        for file in os.listdir(os.path.join(config.data, 'subset' + str(i))):
-            if suffix in file:
-                x_valid.append(os.path.join(config.data, 'subset' + str(i), file))
-    for i in test_fold:
-        for file in os.listdir(os.path.join(config.data, 'subset' + str(i))):
-            if suffix in file:
-                x_test.append(os.path.join(config.data, 'subset' + str(i), file))
+    # for i in train_fold:  # 首先对位于 train_fold 中的数据检查
+    #     for file in os.listdir(os.path.join(config.data, "subset" + str(i))):
+    #         if suffix in file:
+    #             if file_list is not None and file.split("_")[0] in file_list:
+    #                 x_train.append(os.path.join(config.data, "subset" + str(i), file))
+    #             elif file_list is None:
+    #                 x_train.append(os.path.join(config.data, "subset" + str(i), file))
+    # for i in valid_fold:
+    #     for file in os.listdir(os.path.join(config.data, "subset" + str(i))):
+    #         if suffix in file:
+    #             x_valid.append(os.path.join(config.data, "subset" + str(i), file))
+    # for i in test_fold:
+    #     for file in os.listdir(os.path.join(config.data, "subset" + str(i))):
+    #         if suffix in file:
+    #             x_test.append(os.path.join(config.data, "subset" + str(i), file))
+
+    for file in os.listdir(config.data):
+        if suffix in file:
+            if file.split(suffix)[0] in file_list:
+                x_train.append(os.path.join(config.data, file))
+                # elif file_list is None:
+            else:
+                x_valid.append(os.path.join(config.data, file))
+                x_test.append(os.path.join(config.data, file))
+
     return x_train, x_valid, x_test
 
 
@@ -89,7 +123,7 @@ class Cutout(object):
             x1 = np.clip(x - self.length // 2, 0, w)
             x2 = np.clip(x + self.length // 2, 0, w)
 
-            mask[y1: y2, x1: x2] = 0.
+            mask[y1:y2, x1:x2] = 0.0
 
         mask = torch.from_numpy(mask)
         mask = mask.expand_as(img)
@@ -109,9 +143,9 @@ def adjust_learning_rate(epoch, args, optimizer):
     #     for param_group in optimizer.param_groups:
     #         param_group['lr'] = new_lr
     lr = args.lr
-    lr *= 0.5 * (1. + math.cos(math.pi * epoch / args.epochs))
+    lr *= 0.5 * (1.0 + math.cos(math.pi * epoch / args.epochs))
     for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
+        param_group["lr"] = lr
 
 
 class AverageMeter(object):
@@ -136,10 +170,11 @@ class AverageMeter(object):
         self.count += n
         self.avg = self.sum / self.count
 
+
 class GaussianBlur(object):
     """Gaussian blur augmentation in SimCLR https://arxiv.org/abs/2002.05709"""
 
-    def __init__(self, sigma=[.1, 2.]):
+    def __init__(self, sigma=[0.1, 2.0]):
         self.sigma = sigma
 
     def __call__(self, x):
